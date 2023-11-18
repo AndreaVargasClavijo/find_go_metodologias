5c52489bc8163ebffc21bf59565277fd72c740e7
andrea vargas <landreav312@gmail.com>
2023-11-18 18:28:11 -0500

    avance

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const openModal = document.querySelector('.modal__ul');
+{const openModal = document.querySelector('.modal__ul');
 const modal = document.querySelector('.modal')
 const closeModal = document.querySelector('.modal__close')
 
@@ -12,9 +12,48 @@ openModal.addEventListener('click', (e)=>{
 closeModal.addEventListener('click', (e)=>{
     e.preventDefault();
     modal.classList.remove('modal--show');
+}) 
+
+}
+
+{
+    
+const openModal = document.querySelector('.modal__ul_2');
+const modal = document.querySelector('.modal_2')
+const closeModal = document.querySelector('.modal__close_2')
+
+
+openModal.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal.classList.add('modal--show');
 })
 
 
+closeModal.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal.classList.remove('modal--show');
+}) 
 
+}
 
+{
+    
+    const openModal = document.querySelector('.modal__ul_3');
+    const modal = document.querySelector('.modal_3')
+    const closeModal = document.querySelector('.modal__close_3')
+    
+    
+    openModal.addEventListener('click', (e)=>{
+        e.preventDefault();
+        modal.classList.add('modal--show');
+    })
+    
+    
+    closeModal.addEventListener('click', (e)=>{
+        e.preventDefault();
+        modal.classList.remove('modal--show');
+    }) 
+    
+    }
 
+    
